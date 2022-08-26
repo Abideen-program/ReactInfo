87fe3639795e008403434f66d44fdf5a685784d2
Abideen Olafimihan <olafimihana@gmail.com>
2022-08-26 11:18:23 +0100

    practicing JSX

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-ReactDOM.render(<ul>
-                    <li>Good</li>
-                    <li>Bad</li>
-                </ul>, document.getElementById('root')
-                )
+const navBar = (
+    <nav>
+        <h1>SOAStore.</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+
+ReactDOM.render(navBar, document.getElementById('root'))
